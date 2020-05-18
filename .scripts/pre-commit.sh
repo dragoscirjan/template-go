836e2fe6e39e7144174d96bf606494acfe32b513
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-05-18 20:51:02 +0300

    +

--- .scripts/pre-commit.sh
+++ .scripts/pre-commit.sh
@@ -22,7 +22,7 @@ STAGED_GO_FILES=$(git diff --cached --name-only | grep ".go$")
 # STAGED_GO_FILES=$(git diff --name-only | grep ".go$")
 
 # DO NOT USE THIS FOR DEV
-# STAGED_GO_FILES=$(find . -iname "*_test.go")
+echo $@ | grep "\-\-all" > /dev/null && STAGED_GO_FILES=$(find ./src -iname "*.go")
 
 if [[ "$STAGED_GO_FILES" = "" ]]; then
     exit 0
@@ -36,36 +36,36 @@ GOCRITIC=$GOPATH/src/github.com/go-critic/go-critic/gocritic
 
 # Check for golint
 if [[ ! -x "$GOLINT" ]]; then
-    printf ">> \033[31mPlease install golint\033[0m (go get -u golang.org/x/lint/golint)"
+    printf ">> \e[33;31mPlease install golint\e[m (go get -u golang.org/x/lint/golint)"
     exit 1
 fi
 
 # Check for goimports
 if [[ ! -x "$GOIMPORTS" ]]; then
-    printf "\t\033[31mPlease install goimports\033[0m (go get golang.org/x/tools/cmd/goimports)"
+    printf "\t\e[33;31mPlease install goimports\e[m (go get golang.org/x/tools/cmd/goimports)"
     exit 1
 fi
 
 # Check for gocyclo
 if [[ ! -x "$GOCYCLO" ]]; then
-    printf "\t\033[31mPlease install gocyclo\033[0m (go get -v github.com/fzipp/gocyclo/...)"
+    printf "\t\e[33;31mPlease install gocyclo\e[m (go get -v github.com/fzipp/gocyclo/...)"
     exit 1
 fi
 
 # Check for gocritic
 # if [[ ! -x "$GOCRITIC" ]] && [[ $MACHINE_OS != "Windows" ]]; then
 if [[ ! -x "$GOCRITIC" ]]; then
-    printf "\t\033[31mPlease install go-critic\033[0m (go get -v github.com/go-lintpack/lintpack/... && go get -v github.com/go-critic/go-critic/...)"
+    printf "\t\e[33;31mPlease install go-critic\e[m (go get -v github.com/go-lintpack/lintpack/... && go get -v github.com/go-critic/go-critic/...)"
     exit 1
 fi
 
 PASS=true
 
 for FILE in $STAGED_GO_FILES; do
-    printf ">> \033[32m$FILE\033[0m"
+    printf ">> \e[33;32m$FILE\e[m"
 
     if [[ ! -f $FILE ]]; then
-        printf ">> \033[32m$FILE\033[93m missing. Probably deleted. Will skip\033[0m\n"
+        printf ">> \e[33;32m$FILE\033[93m missing. Probably deleted. Will skip\e[m\n"
         echo
         continue
     else
@@ -78,15 +78,16 @@ for FILE in $STAGED_GO_FILES; do
 
     # Run gofmt on the staged file
     COMMAND="gofmt -l -w $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m$COMMAND\e[m ... "
     $COMMAND
-    printf "\033[32mOK\033[0m\n"
+    printf "\e[33;32mOK\e[m\n"
 
     # Run goimports on the staged file
     COMMAND="$GOIMPORTS -w $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m%s\e[m ... " "$COMMAND"
+    # printf '\e[1;34m%-6s\e[m' "This is text"
     $COMMAND
-    printf "\033[32mOK\033[0m\n"
+    printf "\e[33;32mOK\e[m\n"
 
     #
     # Linting
@@ -94,38 +95,38 @@ for FILE in $STAGED_GO_FILES; do
 
     # Run golint on the staged file and check the exit status
     COMMAND="$GOLINT -set_exit_status $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m%s\e[m ... " "$COMMAND"
     $COMMAND &> /tmp/__pre_commit_go__
     if [[ $? == 1 ]]; then
-        printf "\033[31mFAILURE!\033[0m\n"
-        printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
+        printf "\e[33;31mFAILURE!\e[m\n"
+        printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
         PASS=false
     else
-        printf "\033[32mOK\033[0m\n"
+        printf "\e[33;32mOK\e[m\n"
     fi
 
     # Run govet on the staged file and check the exit status
     COMMAND="go vet $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m$COMMAND\e[m ... "
     $COMMAND &> /tmp/__pre_commit_go__
     if [[ $? == 1 ]]; then
-        printf "\033[31mFAILURE!\033[0m\n"
-        printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
+        printf "\e[33;31mFAILURE!\e[m\n"
+        printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
         PASS=false
     else
-        printf "\033[32mOK\033[0m\n"
+        printf "\e[33;32mOK\e[m\n"
     fi
 
     # # Run golangci-lint on the staged file and check the exit status
     # COMMAND="$GOLANGCILINT run $FILE"
-    # printf "\t\033[90m$COMMAND\033[0m ... "
+    # printf "\t\e[33;90m$COMMAND\e[m ... "
     # $COMMAND #&> /tmp/__pre_commit_go__
     # if [[ $? == 1 ]]; then
-    #     printf "\033[31mFAILURE!\033[0m\n"
-    #     printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
+    #     printf "\e[33;31mFAILURE!\e[m\n"
+    #     printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
     #     PASS=false
     # else
-    #     printf "\033[32mOK\033[0m\n"
+    #     printf "\e[33;32mOK\e[m\n"
     # fi
 
     #
@@ -134,30 +135,30 @@ for FILE in $STAGED_GO_FILES; do
 
     # Run gocyclo on the staged file and check the exit status
     COMMAND="$GOCYCLO -over=15 $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m%s\e[m ... " "$COMMAND"
     $COMMAND &> /tmp/__pre_commit_go__
     if [[ $? == 1 ]]; then
-        printf "\033[31mFAILURE!\033[0m\n"
-        printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
+        printf "\e[33;31mFAILURE!\e[m\n"
+        printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
         PASS=false
     else
-        printf "\033[32mOK\033[0m\n"
+        printf "\e[33;32mOK\e[m\n"
     fi
 
     # Run gocritic on the staged file and check the exit status
     COMMAND="$GOCRITIC check $FILE"
-    printf "\t\033[90m$COMMAND\033[0m ... "
+    printf "\t\e[33;90m%s\e[m ... " "$COMMAND"
     # if [[ $MACHINE_OS != "Windows" ]]; then
-        echo $COMMAND &> /tmp/__pre_commit_go__
-        if [[ $? == 1 ]]; then
-            printf "\033[31mFAILURE!\033[0m\n"
-            printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
-            PASS=false
-        else
-            printf "\033[32mOK\033[0m\n"
-        fi
+    echo $COMMAND &> /tmp/__pre_commit_go__
+    if [[ $? == 1 ]]; then
+        printf "\e[33;31mFAILURE!\e[m\n"
+        printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
+        PASS=false
+    else
+        printf "\e[33;32mOK\e[m\n"
+    fi
     # else
-    #     printf "\033[32mSkiping on Windows\033[0m\n"
+    #     printf "\e[33;32mSkiping on Windows\e[m\n"
     # fi
 
     #
@@ -165,18 +166,18 @@ for FILE in $STAGED_GO_FILES; do
     #
     echo $FILE | grep "_test.go" &> /dev/null
     if [[ $? -eq 0 ]]; then
-        # printf "\033[31mUnit testing has been disabled until SMB\\Guest user issues is solved.\033[0m\n"
+        # printf "\e[33;31mUnit testing has been disabled until SMB\\Guest user issues is solved.\e[m\n"
         cd $(dirname $FILE)
         # Run gocritic on the staged file and check the exit status
         COMMAND="go test -tags=unit -timeout 30s -short -v"
-        printf "\t\033[90m$COMMAND\033[0m ... "
+        printf "\t\e[33;90m$COMMAND\e[m ... "
         $COMMAND &> /tmp/__pre_commit_go__
         if [[ $? != 0  ]]; then
-            printf "\033[31mFAILURE!\033[0m\n"
-            printf "\033[31m$(cat /tmp/__pre_commit_go__)\033[0m\n"
+            printf "\e[33;31mFAILURE!\e[m\n"
+            printf "\e[33;31m$(cat /tmp/__pre_commit_go__)\e[m\n"
             PASS=false
         else
-            printf "\033[32mOK\033[0m\n"
+            printf "\e[33;32mOK\e[m\n"
         fi
         cd $RUNNING_PATH
     fi
@@ -196,10 +197,10 @@ echo
 echo
 
 if ! $PASS; then
-    printf "\033[0;30m\033[31mCOMMIT FAILED\033[0m\n"
+    printf "\033[0;30m\e[33;31mCOMMIT FAILED\e[m\n"
     exit 1
 else
-    printf "\033[0;30m\033[42mCOMMIT SUCCEEDED\033[0m\n"
+    printf "\033[0;30m\033[42mCOMMIT SUCCEEDED\e[m\n"
 fi
 
 echo
